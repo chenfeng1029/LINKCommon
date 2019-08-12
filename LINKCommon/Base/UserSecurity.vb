4b7871755dd2c8a7ac20d64f79318bd8cf8385ca
kevin zhu <chenfeng1029@hotmail.com>
2019-08-12 11:34:45 +0800

    用户身份验证结构类型

---
+++ LINKCommon/Base/UserSecurity.vb
@@ -0,0 +1,12 @@
+﻿Namespace Base
+    ''' <summary>
+    ''' 创建时间：2019.8.12
+    ''' 说明:类调用身份验证结构类  
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Structure UserSecurity
+        Public UserName As String '//用户名
+        Public SecuriKey As String '//类验证码
+        Public ProcKey As String '//类过程及函数验证
+    End Structure
+End NameSpace
